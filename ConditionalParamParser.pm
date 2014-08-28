101982260c971dd88c0ed975517a51fc6ec4f5f8
Greg Hanneman <ghanneman@gmail.com>
2014-08-28 08:54:10 -0700

    New generalized scoring script

---
+++ ConditionalParamParser.pm
@@ -0,0 +1,196 @@
+package ConditionalParamParser;
+@EXPORT = qw(GetParams);
+
+use strict;
+
+
+# Official flag names to specify which columns are in the numerator and which
+# are in the denominator of conditional probability scores:
+my $SCORE_FLAG = "--score";     # numerator
+my $COND_FLAG = "--cond";       # denominator
+my $TYPE_FLAG = "--type";       # type of score to compute
+my $NAME_FLAG = "--name";       # name of this feature
+my $SNFILE_FLAG = "--sn-file";  # file of score names
+# Official column names, to be used as arguments to the above:
+# NOTE: Indexes are 0-based for ease of Perl-internal use!
+my %Columns = (
+    "type" => 0,
+    "lhs" => 1,
+    "src-rhs" => 2,
+    "tgt-rhs" => 3,
+    );
+
+# Official type names, to determine which kind of score to compute:
+my %Types = (
+    "prob" => "compute p($SCORE_FLAG | $COND_FLAG)",
+    "counts" => "compute #($SCORE_FLAG, $COND_FLAG) and #($COND_FLAG)",
+    "entropy" => "compute H($SCORE_FLAG) within $COND_FLAG",
+    "gain" => "x",
+    );
+
+
+sub PrintUsage
+{
+    print STDERR "Usage:\n";
+    print STDERR "    cat <rules> | \\\n";
+    print STDERR "    perl $0 \\\n";
+    print STDERR "        [$SCORE_FLAG <col-name>]+ \\\n";
+    print STDERR "        [$COND_FLAG <col-name>]+ \\\n";
+    print STDERR "        $TYPE_FLAG <type> \\\n";
+    print STDERR "        $NAME_FLAG <feat-name>\n";
+    print STDERR "        $SNFILE_FLAG <sn-file>\n";
+    print STDERR "Column names for <col-name>:\n";
+    foreach my $col (keys %Columns) { print STDERR "    $col\n"; }
+    print STDERR "Type names for <type>:\n";
+    foreach my $type (keys %Types)
+    {
+	print STDERR "    $type : $Types{$type}\n";
+    }
+    print STDERR "\nComputes the feature indicated by <type> and calls it <feat-name>.  The input\n";
+    print STDERR "file <sn-file> contains a space-separated list of scores already appearing\n";
+    print STDERR "in <rules>; it will be OVERWRITTEN with an updated list upon output.\n\n";
+    print STDERR "WARNING:  Input <rules> must be sorted by the $COND_FLAG columns!\n";
+}
+
+
+sub GetParams
+{
+    # Get function parameters:
+    my @Args = @_;
+
+    # Read through input args one by one; collect lists of numerator and
+    # denominator column numbers, along with feature name and score type:
+    my @NumCols = ();
+    my @DenomCols = ();
+    my $name = "";
+    my $scoreType = "";
+    my $snFile = "";
+    for(my $i = 0; $i <= $#Args; $i++)
+    {
+	if($Args[$i] eq $SCORE_FLAG)
+	{
+	    # Found a numerator column:
+	    $i++;
+	    if(exists($Columns{$Args[$i]}))
+	    {
+		# Add this column to the list of numerator columns:
+		push(@NumCols, $Columns{$Args[$i]});
+	    }
+	    else
+	    {
+		print STDERR "ERROR:  Invalid column name '$Args[$i]'.\n";
+		PrintUsage();
+		exit(1);
+	    }
+	}
+	elsif($Args[$i] eq $COND_FLAG)
+	{
+	    # Found a denominator column:
+	    $i++;
+	    if(exists($Columns{$Args[$i]}))
+	    {
+		# Add this column to the list of denominator columns:
+		push(@DenomCols, $Columns{$Args[$i]});
+	    }
+	    else
+	    {
+		print STDERR "ERROR:  Invalid column name '$Args[$i]'.\n";
+		PrintUsage();
+		exit(1);
+	    }
+	}
+	elsif($Args[$i] eq $NAME_FLAG)
+	{
+	    # Found the name of this feature:
+	    $i++;
+	    if($name eq "") { $name = $Args[$i]; }
+	    else
+	    {
+		print STDERR "ERROR:  Tried to name feature twice ('$name' vs. '$Args[$i]').  Pick just one!\n";
+		PrintUsage();
+		exit(1);
+	    }
+	}
+	elsif($Args[$i] eq $TYPE_FLAG)
+	{
+	    # Found the type of this feature:
+	    $i++;
+	    if(exists($Types{$Args[$i]}))
+	    {
+		if($scoreType eq "") { $scoreType = $Args[$i]; }
+		else
+		{
+		    print STDERR "ERROR:  Tried to set score type twice ('$scoreType' vs. '$Args[$i]').  Pick just one!\n";
+		    PrintUsage();
+		    exit(1);
+		}
+	    }
+	    else
+	    {
+		print STDERR "ERROR:  Invalid type name '$Args[$i]'.\n";
+		PrintUsage();
+		exit(1);
+	    }
+	}
+	elsif($Args[$i] eq $SNFILE_FLAG)
+	{
+	    # Found the name of the score-names files:
+	    $i++;
+	    if($snFile eq "") { $snFile = $Args[$i]; }
+	    else
+	    {
+		print STDERR "ERROR:  Tried to specify the score-names file twice ('$snFile' vs. '$Args[$i]').  Pick just one!\n";
+		PrintUsage();
+		exit(1);
+	    }
+	}
+	else
+	{
+	    # Found something unexpected:
+	    print STDERR "ERROR:  Invalid argument '$Args[$i]'.\n";
+	    PrintUsage();
+	    exit(1);
+	}
+    }
+
+    # Now we have the column lists: make sure at least one in each:
+    if($#NumCols < 0 || $#DenomCols < 0)
+    {
+	print STDERR "ERROR:  Must specify at least one column each for $SCORE_FLAG and $COND_FLAG.\n";
+	PrintUsage();
+	exit(1);
+    }
+
+    # And we want a feature name, score type, and score-names file too:
+    if($name eq "")
+    {
+	print STDERR "ERROR:  Must specify feature name ($NAME_FLAG).\n";
+	PrintUsage();
+	exit(1);
+    }
+    if($scoreType eq "")
+    {
+	print STDERR "ERROR:  Must specify score type ($TYPE_FLAG).\n";
+	PrintUsage();
+	exit(1);
+    }
+    if($snFile eq "")
+    {
+	print STDERR "ERROR:  Must specify score-names file ($SNFILE_FLAG).\n";
+	PrintUsage();
+	exit(1);
+    }
+
+    # Pack into hash to return:
+    @NumCols = sort {$a <=> $b} @NumCols;
+    @DenomCols = sort {$a <=> $b} @DenomCols;
+    # TEMP DEBUG BLOCK:
+    print STDERR "num : [@NumCols]\n";
+    print STDERR "denom : [@DenomCols]\n";
+    print STDERR "name : [$name]\n";
+    print STDERR "type : [$scoreType]\n";
+    print STDERR "snfile : [$snFile]\n";
+    ###################
+    return ('num' => "@NumCols", 'denom' => "@DenomCols", 'name' => $name,
+	    'type' => $scoreType, 'snfile' => $snFile);
+}
---
+++ score-conditional.pl
@@ -0,0 +1,173 @@
+use lib "/home/ghannema/tools/git/grascore";
+use ConditionalParamParser;
+use ScoreUtils;
+use strict;
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+
+
+# Check usage and get parameters hash:
+my %Params = ConditionalParamParser::GetParams(@ARGV);
+
+# Read in list of score names already in rules:
+open(my $FILE, $Params{'snfile'}) or
+    die "Can't open input file $Params{'snfile'}: $!";
+my $line = <$FILE>;
+close($FILE);
+chomp $line;
+my @ScoreNames = split(/\s+/, $line);
+
+# Make sure the rule count is one of the existing score names:
+my $countIndex = -1;
+for my $i (0..$#ScoreNames)
+{
+    if($ScoreNames[$i] eq ScoreUtils::COUNT_SNAME())
+    {
+	$countIndex = $i;
+	last;
+    }
+}
+if($countIndex == -1)
+{
+    print STDERR "ERROR:  Input rules don't have count field, or score names file incorrect.\n";
+    exit(1);
+}
+
+# Figure out scoring function to call and make sure it exists:
+my $scoreFn = $Params{'type'};
+$scoreFn =~ s/^(.)/uc($1)/e;
+if(!exists(&{"Score$scoreFn"}))
+{
+    print STDERR "ERROR:  Could not find expected scoring function 'Score$scoreFn'.\n";
+    exit(1);
+}
+
+# Accumulator for multiple rules with the same denominator columns:
+my %RuleNumerCols = ();
+my %NumerColCounts = ();
+my $currDenom = "";
+my $totalCount = 0;
+
+# Read rule instances from standard in, one per line:
+# NOTE: This assumes rules are sorted by the denominator columns!
+while(my $line = <STDIN>)
+{
+    # Break rule line into fields:
+    chomp $line;
+    my @RuleParts = split(/\t/, $line);
+    my @Scores = split(/\s+/, @RuleParts[-1]);
+
+    # Assemble numerator and denominator columns, separated by tabs:
+    my $numer = join("\t", @RuleParts[split(/\s+/, $Params{'num'})]);
+    my $denom = join("\t", @RuleParts[split(/\s+/, $Params{'denom'})]);
+
+    # If different denominator than previously, write out and score old rules:
+    if($denom ne $currDenom)
+    {
+	# Write out and score old rules:
+	if($totalCount > 0)
+	{
+	    eval "Score$scoreFn" . '(\%RuleNumerCols, \%NumerColCounts, $totalCount);';
+	}
+
+	# Reset accumulator ("undef" seems faster than "= ()"):
+	undef %RuleNumerCols;
+	undef %NumerColCounts;
+	$currDenom = $denom;
+	$totalCount = 0;
+    }
+
+    # Add this rule and its count to the appropriate numerator accumulator:
+    $RuleNumerCols{join("\t", @RuleParts)} = $numer;
+    $NumerColCounts{$numer} += $Scores[$countIndex];
+    $totalCount += $Scores[$countIndex];
+}
+
+# At end, write out final rule still in accumulator:
+if($totalCount > 0)
+{
+    eval "Score$scoreFn" . '(\%RuleNumerCols, \%NumerColCounts, $totalCount);';
+}
+
+# Write out new list of score names:
+open($FILE, "> $Params{'snfile'}") or die "Can't open output file $Params{'snfile'}: $!";
+print $FILE "@ScoreNames $Params{'name'}\n";
+close($FILE);
+
+
+# SUBROUTINES FOR DIFFERENT TYPES OF SCORING: ################################
+
+
+# ScoreProb(\%RulesToNumerCols, \%NumerColsToCounts, $denomCount):
+#    Takes as input (1) a mapping from full rules all sharing the same
+#    denominator value to their numerator values, (2) a mapping from numerator
+#    values to numerator counts, and (3) the total count of the relevant
+#    denominator.  Prints out each of the rules found plus its corresponding
+#    conditional probability.
+sub ScoreProb
+{
+    # Get parameters (Hungarian "h" denotes hash reference):
+    my $hRulesToNumerCols = shift @_;
+    my $hNumerColsToCounts = shift @_;
+    my $denomCount = shift @_;
+
+    # Now compute conditional probability for each rule:
+    foreach my $r (keys %{$hRulesToNumerCols})
+    {
+	my $prob = ${$hNumerColsToCounts}{${$hRulesToNumerCols}{$r}} / $denomCount;
+	print "$r $prob\n";
+    }
+}
+
+
+# ScoreCounts(\%RulesToNumerCols, \%NumerColsToCounts, $denomCount):
+#    Takes as input (1) a mapping from full rules all sharing the same
+#    denominator value to their numerator values, (2) a mapping from numerator
+#    values to numerator counts, and (3) the total count of the relevant
+#    denominator.  Prints out each of the rules found plus the numerator and
+#    denominator counts, in the form "num/denom".
+sub ScoreCounts
+{
+    # Get parameters (Hungarian "h" denotes hash reference):
+    my $hRulesToNumerCols = shift @_;
+    my $hNumerColsToCounts = shift @_;
+    my $denomCount = shift @_;
+
+    # Now compute numerator and denominator counts for each rule:
+    foreach my $r (keys %{$hRulesToNumerCols})
+    {
+	my $num = ${$hNumerColsToCounts}{${$hRulesToNumerCols}{$r}};
+	print "$r $num/$denomCount\n";
+    }
+}
+
+
+# ScoreEntropy(\%RulesToNumerCols, \%NumerColsToCounts, $denomCount):
+#    Takes as input (1) a mapping from full rules all sharing the same
+#    denominator value to their numerator values, (2) a mapping from numerator
+#    values to numerator counts, and (3) the total count of the relevant
+#    denominator.  Prints out each of the rules found plus the entropy over
+#    the probabilities for this denominator.
+sub ScoreEntropy
+{
+    # Get parameters (Hungarian "h" denotes hash reference):
+    my $hRulesToNumerCols = shift @_;
+    my $hNumerColsToCounts = shift @_;
+    my $denomCount = shift @_;
+
+    # Now compute entropy over the unique numerators:
+    my $entropy = 0;
+    foreach my $numer (keys %{$hNumerColsToCounts})
+    {
+	my $prob = ${$hNumerColsToCounts}{$numer} / $totalCount;
+	$entropy = $entropy - ($prob * log($prob) / log(2));
+    }
+
+    # Add it to each rule with this denominator:
+    foreach my $r (keys %{$hRulesToNumerCols})
+    {
+	print "$r $entropy\n";
+    }
+}
